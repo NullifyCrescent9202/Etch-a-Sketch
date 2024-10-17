b0f69766d7a3dac5579b08c164c29638b93cde64
NullifyCrescent9202 <97800784+NullifyCrescent9202@users.noreply.github.com>
2024-10-17 00:21:19 -0400

    Add function that creates a square grid.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
- 
+const grid = document.querySelector(".grid");
+
+function createGrid(gridSize){
+    for(let i = 0; i < gridSize; i++){
+        const row = document.createElement("div");
+        row.className = "row";
+        grid.append(row);
+        
+        for(let j = 0; j < gridSize; j++){
+            const pixel = document.createElement("div");
+            pixel.className= "pixel";
+            row.append(pixel);
+        }
+    }
+}
